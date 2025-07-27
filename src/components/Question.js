fd6b2b22bbd55e19e46719bc97e82fe803c5e6d1
unknown <akaidylan81@gmail.com>
2025-07-27 23:58:43 +0100

    initial

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,31 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
-  const [timeRemaining, setTimeRemaining] = useState(10);
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setSecondsRemaining((prev) => prev - 1);
+    }, 1000);
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+    // when time runs out, call onAnswered with false
+    if (secondsRemaining === 0) {
+      onAnswered(false);
+    }
 
-  const { id, prompt, answers, correctIndex } = question;
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [secondsRemaining, onAnswered]);
 
   return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    <div>
+      <h2>{question.prompt}</h2>
+      <p>{secondsRemaining} seconds remaining</p>
+    </div>
   );
 }
 
